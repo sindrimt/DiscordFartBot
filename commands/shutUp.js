ccdc7d051f4587dcb1ef40ac5657897f69a6c441
sindrimt <sindrimt@stud.ntnu.no>
2021-09-19 19:32:15 +0200

    sdf

--- commands/shutUp.js
+++ commands/shutUp.js
@@ -7,7 +7,7 @@ const eventEmitter = new EventEmitter()
 module.exports = {
     name: "shutup",
     description: "Leaves channel",
-    aliases: ["stfu"],
+    aliases: ["stfu", "die", "fuckoff", "pissoffidiot"],
     category: ["music"],
     execute: async (client, message) => {
         message.channel.send("Okay masta :disappointed_relieved: ");
