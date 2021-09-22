649fd88db66f32c1e259b58ba38314ace2e80fb2
sindrimt <sindrimt@stud.ntnu.no>
2021-09-22 18:42:50 +0200

    sdf

--- commands/skip.js
+++ commands/skip.js
@@ -2,18 +2,17 @@ const config = require("../Data/config.json");
 const { distube } = require("./play");
 
 module.exports = {
-    name: "skip",
-    description: "Skips song",
-    aliases: ["s"],
-    category: ["music"],
-    execute: async (client, message) => {
-
-        const queue = distube.getQueue(message);
-        if (queue.songs.length >= 2) {
-            queue.skip();
-        } else {
-            message.channel.send(`Empty queue ${config.emotes.sob}`);
-        }
-    },
+  name: "skip",
+  description: "Skips song",
+  aliases: ["s"],
+  category: ["music"],
+  execute: async (client, message) => {
+    const queue = distube.getQueue(message);
+    if (queue.songs.length >= 2) {
+      queue.skip();
+    } else {
+      message.channel.send(`Empty queue ${config.emotes.sob}`);
+      queue.stop();
+    }
+  },
 };
-
